b48e1b66dba66ccb868ecdbed63ec6c2b50a5595
Matt <mgudwin@gmail.com>
2024-12-21 17:43:30 -0500

    d8 still wrong

--- day8.py
+++ day8.py
@@ -41,7 +41,14 @@ def assemble_list_of_antenna(df):
                     antenna_dict[cell] = [(r, c)]
     return antenna_dict
 
+def check_if_in_bounds(point, num_row, num_col):
+    return (
+        ((point[0] >= 0) and (point[0] <= num_col)) and 
+        ((point[1] >= 0) and (point[1] <= num_row))         
+        )
+
 def calculate_antinodes(antenna_locations, df):
+    num_row, num_col = df.shape
     antinote_location = {}
     for key in antenna_locations:
         length_of_points = len(antenna_locations[key])
@@ -52,22 +59,17 @@ def calculate_antinodes(antenna_locations, df):
                 p1 = antenna_locations[key][ix]
                 p2 = antenna_locations[key][ix2]
                 p21, p12, anode1, anode2 = calc_antinode_locations(p1, p2)
-                try:
-                    if df.loc[anode1[0], anode1[1]] == ".":
-                        if key in antinote_location:
-                            antinote_location[key].append(anode1)
-                        else:
-                            antinote_location[key] = [anode1]
-                except:
-                    continue
-                try:
-                    if df.loc[anode2[0], anode2[1]] == ".":
-                        if key in antinote_location:
-                            antinote_location[key].append(anode2)
-                        else:
-                            antinote_location[key] = [anode2]
-                except:
-                    continue
+                
+                if check_if_in_bounds(anode1, num_row, num_col):
+                    if key in antinote_location:
+                        antinote_location[key].append(anode1)
+                    else:
+                        antinote_location[key] = [anode1]
+                if check_if_in_bounds(anode2, num_row, num_col):
+                    if key in antinote_location:
+                        antinote_location[key].append(anode2)
+                    else:
+                        antinote_location[key] = [anode2]
     return antinote_location
 
 
