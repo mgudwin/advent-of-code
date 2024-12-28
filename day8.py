6e3bde77050a998d40b50ad138613ef2feff287f
Matt Gudwin <matt@Matts-MacBook-Pro.local>
2024-12-27 19:11:35 -0500

    day8 done

--- day8.py
+++ day8.py
@@ -1,99 +1,107 @@
 import pandas as pd
 import numpy as np
 
+
+class city_scape():
+    def __init__(self):
+        self.map = None
+        self.map_dimensions = None
+        self.antenna_locations = {}
+        self.antinode_locations = []
+
+    def read_map(self, file):
+        with open(file, 'r') as file:
+            matrix = []
+            for line in file:
+                matrix.append(list(line))
+        self.map = pd.DataFrame(data=matrix)
+        self.map.drop(self.map.columns[-1], axis=1, inplace=True)
+        self.map_dimensions = self.map.shape
+
+    def print_map(self, ant=False):
+        if ant:
+            temp = self.map.copy()
+            for location in self.antinode_locations:
+                temp.loc[location[0], location[1]] = "#"
+            print(temp)
+        else:
+            print(self.map)
+
+    def assemble_list_of_antenna(self):
+        for r, row in self.map.iterrows():
+            for c, cell in enumerate(row):
+                if cell == ".":
+                    continue
+                else:
+                    if cell in self.antenna_locations:
+                        self.antenna_locations[cell].append((r, c))
+                    else:
+                        self.antenna_locations[cell] = [(r, c)]
+
+    def return_antinode_group(self, p1, p2):
+        vector = np.subtract(p2, p1)
+        anode1 = np.subtract(p1, vector)
+        anode2 = np.add(p2, vector)
+        return (anode1, anode2)
+
+    def return_all_antinodes(self, p1, p2):
+        anode_list = []
+        vector = np.subtract(p2, p1)
+        anode1 = np.subtract(p1, vector)
+        while self.check_if_in_bounds(anode1):
+            anode1 = np.subtract(anode1, vector)
+            if self.check_if_in_bounds(anode1):
+                anode_list.append(anode1)
+        anode2 = np.add(p2, vector)
+        while self.check_if_in_bounds(anode2):
+            anode2 = np.add(anode2, vector)
+            if self.check_if_in_bounds(anode2):
+                anode_list.append(anode2)
+        return (anode_list)
+
+    def check_if_in_bounds(self, anode):
+        num_row, num_col = self.map_dimensions
+        return (anode[0] >= 0) and (anode[0] < num_row) and\
+            (anode[1] >= 0) and (anode[1] < num_col)
+
+    def calculate_antinodes(self, max_count=2):
+        for frequency in self.antenna_locations:
+            num_of_antennas = len(self.antenna_locations[frequency])
+            for ix in range(num_of_antennas):
+                for ix2 in range(num_of_antennas):
+                    if ix == ix2:
+                        continue
+                    p1 = self.antenna_locations[frequency][ix]
+                    p2 = self.antenna_locations[frequency][ix2]
+                    anode1, anode2 = self.return_antinode_group(p1, p2)
+                    if self.check_if_in_bounds(anode1):
+                        self.antinode_locations.append(anode1)
+                    if self.check_if_in_bounds(anode2):
+                        self.antinode_locations.append(anode2)
+                    if max_count > 2:
+                        self.antinode_locations.append(p1)
+                        self.antinode_locations.append(p2)
+                        for anode in self.return_all_antinodes(p1, p2):
+                            if self.check_if_in_bounds(anode):
+                                self.antinode_locations.append(anode)
+
+    def calculate_distinct_nodes(self):
+        print("There are {} disctinct antinode locations".format(
+            len(np.unique(self.antinode_locations, axis=0))))
+
+
 # ###########################################################
 # Inputs
 # ###########################################################
 # file = "Inputs/day8_example.txt"
 file = "Inputs/day8_input.txt"
-debug_file = "Inputs/day8_debug.txt"
 
-with open(file, 'r') as file:
-    matrix = []
-    for line in file:
-        matrix.append(list(line))
-
-df = pd.DataFrame(data=matrix)
-df.drop(df.columns[-1], axis=1, inplace=True)
-print(df)
-
-# ###########################################################
-# Process
-# ###########################################################
-
-def calc_antinode_locations(p1, p2):
-    p21 = np.subtract(p2, p1)
-    p12 = np.subtract(p1, p2)
-    anode1 = p1 + p12
-    anode2 = p2 + p21
-    return (p21, p12, anode1, anode2)
-
-def assemble_list_of_antenna(df):
-    antenna_dict = {}
-    for r, row in df.iterrows():
-        for c, cell in enumerate(row):
-            if cell == ".":
-                continue
-            else:
-                # check if in antenna_dict
-                if cell in antenna_dict:
-                    antenna_dict[cell].append((r, c))
-                else:
-                    antenna_dict[cell] = [(r, c)]
-    return antenna_dict
-
-def check_if_in_bounds(point, num_row, num_col):
-    return (
-        ((point[0] >= 0) and (point[0] < num_col)) and 
-        ((point[1] >= 0) and (point[1] < num_row))         
-        )
-
-def calculate_antinodes(antenna_locations, df):
-    num_row, num_col = df.shape
-    antinote_location = {}
-    for key in antenna_locations:
-        length_of_points = len(antenna_locations[key])
-        for ix in range(length_of_points):
-            for ix2 in range(length_of_points):
-                if ix == ix2:
-                    continue
-                p1 = antenna_locations[key][ix]
-                p2 = antenna_locations[key][ix2]
-                p21, p12, anode1, anode2 = calc_antinode_locations(p1, p2)
-                
-                if check_if_in_bounds(anode1, num_row, num_col):
-                    if key in antinote_location:
-                        antinote_location[key].append(anode1)
-                    else:
-                        antinote_location[key] = [anode1]
-                if check_if_in_bounds(anode2, num_row, num_col):
-                    if key in antinote_location:
-                        antinote_location[key].append(anode2)
-                    else:
-                        antinote_location[key] = [anode2]
-    return antinote_location
-
-
-antenna_locations = assemble_list_of_antenna(df)
-node_locations = calculate_antinodes(antenna_locations, df)
-
-# ###########################################################
-# Post Process
-# ###########################################################
-
-debug_df = df.copy()
-
-unique_list = []
-ant_str_list = []
-for key in node_locations:
-    for ix, location in enumerate((node_locations[key])):
-        debug_df.loc[location[0], location[1]] = "#"
-        unique_list.append("{}".format("-".join(np.char.mod('%d', location))))
-        ant_str_list.append("{} : {}".format(key, "-".join(np.char.mod('%d', location))))
-
-for ix, p in enumerate(set(ant_str_list)):
-    print("{} - {}".format(ix, p))
-
-print("Done, there are {} unique antinodes".format(len(set(ant_str_list))))
-print("There are {} totally unique antinodes".format(len(set(unique_list))))
- 
+city = city_scape()
+city.read_map(file)
+print('Printing whole map')
+city.print_map(ant=False)
+city.assemble_list_of_antenna()
+city.calculate_antinodes(max_count=10)
+print('Printing antinode locations')
+city.print_map(ant=True)
+city.calculate_distinct_nodes()
