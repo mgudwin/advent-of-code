ea80fa6123adaee098d11bea31f4fb82696ac5ca
Matt Gudwin <matt@Matts-MacBook-Pro.local>
2025-01-09 12:13:29 -0500

    D10P1 done

--- day10.py
+++ day10.py
@@ -1,95 +1,105 @@
-import pandas as pd
+from collections import deque, defaultdict
+
+# from typing import List
+from copy import deepcopy
+from tabulate import tabulate
 
 # Can only move up, down, left, right
 # trailhead starts at 0 and scored by however many 9s you can reaach from it
 
 
-class Map():
+class Map:
     def __init__(self, file):
-        with open(file, 'r') as file:
-            matrix = []
-            for line in file:
-                matrix.append(list(line)[:-1])
-        self.map = pd.DataFrame(data=matrix)
-        self.print_map()
-        self.peaks = self.find_peaks()
+        with open(file, "r") as file:
+            contents = file.read()
+        rows = contents.strip().split("\n")
+        self.map = [list(x) for x in rows]
+        self.map = [[int(char) for char in row] for row in self.map]
+        self.ncols = len(self.map)
+        self.nrows = len(self.map[0])
+        self.headers = [str(i) for i in range(self.ncols)]
+        self.print_map(self.map)
+        self.trailheads = self.find_trailheads()
 
-    def print_map(self):
+    def print_map(self, map):
         print("Printing map...")
-        print(self.map)
+        print(tabulate(map, tablefmt="plain", headers=self.headers, showindex=True))
 
-    def find_peaks(self):
-        peak_map = self.map.copy()
-        list_of_peaks = []
-        for col in peak_map.columns.to_list():
-            for ix in peak_map[peak_map[col] == "9"].index.to_list():
-                list_of_peaks.append([int(ix), int(col)])
-        print("Found the following peaks:")
-        for peak in list_of_peaks:
-            peak_map.loc[peak[0], peak[1]] = "X"
-        print(peak_map)
-        return list_of_peaks
+    def find_trailheads(self):
+        trailhead_map = deepcopy(self.map)
+        list_of_trailheads = []
+        for row in range(len(trailhead_map)):
+            for col in range(len(trailhead_map[0])):
+                if trailhead_map[row][col] == 0:
+                    list_of_trailheads.append([row, col])
+        return list_of_trailheads
 
-    def print_peak_list(self):
-        print("List of peaks are:")
-        for ix, peak in enumerate(self.peaks):
-            print("{} - {}".format(ix, peak))
+    def print_trailhead_list(self):
+        print("List of trailheads in [row, col] format are:")
+        for trailhead in self.trailheads:
+            print("{}".format(trailhead))
 
-    def process_peak(self, peak):
-        map = self.map.copy()
-        # Get current position
-        current_point = peak
-        current_value = int(map.iat[current_point[0], current_point[1]])
-        print(current_point, current_value)
-        if current_value != 9:
-            print("Peak does not end at 9, starts at", current_value)
-            return 0
-        bounds = self.check_bounds(current_point)
-        print('bounds', bounds)
-        # Find next number
-        if bounds["up"]:
-            up_num = int(map.iat[current_point[0] - 1, current_point[1]])
-            print("Up in bounds and value is {}".format(up_num))
-            if up_num == current_value - 1:
-                print("Up decreases")
-        if bounds["down"]:
-            down_num = int(map.iat[current_point[0] + 1, current_point[1]])
-            print("Down in bounds and value is {}".format(down_num))
-            if down_num == current_value - 1:
-                print("Down decreases")
-        if bounds["left"]:
-            left_num = int(map.iat[current_point[0], current_point[1] - 1])
-            print("Left in bounds and value is {}".format(left_num))
-            if left_num == current_value - 1:
-                print("Left decreases")
-        if bounds["right"]:
-            right_num = int(map.iat[current_point[0], current_point[1] + 1])
-            print("Right in bounds and value is {}".format(right_num))
-            if right_num == current_value - 1:
-                print("Right decreases")
-        # Complete loop and note if there's another route
-        # Stop if hit 9 or can't find next number
+    def process_trailhead(self, trailhead_loc: list):
+        queue = deque()
+        nines = []
+        visited = {}
+        visited[str(trailhead_loc)] = True
+        queue.append(trailhead_loc)
+        neighbors = []
+        while queue:
+            curr_point = queue.popleft()
+            curr_value = self.map[curr_point[0]][curr_point[1]]
+            neighbors = self.get_neighbors(curr_point)
+            # print(neighbors)
+            for neighbor in neighbors:
+                # print("Processing neighbor", neighbor)
+                neighbor_value = self.map[neighbor[0]][neighbor[1]]
+                if neighbor_value == curr_value + 1:
+                    if neighbor_value == 9:
+                        nines.append(str(neighbor))
+                    if str(neighbor) not in visited:
+                        visited[str(neighbor)] = True
+                        queue.append(neighbor)
+        nines = list(set(nines))
+        return nines
+
+    def get_neighbors(self, current_point):
+        neighbors = []
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for dir in directions:
+            c = [current_point[0] + dir[0], current_point[1] + dir[1]]
+            if self.check_bounds(c):
+                neighbors.append(c)
+        return neighbors
 
     def check_bounds(self, point):
-        nrows, ncols = self.map.shape
-        move_up = True
-        move_down = True
-        move_left = True
-        move_right = True
-        if point[0] == 0:
-            move_up = False
-        if point[0] == nrows:
-            move_down = False
-        if point[1] == 0:
-            move_left = False
-        if point[1] == ncols:
-            move_right = False
-        return {"left": move_left, "right": move_right,
-                "up": move_up, "down": move_down}
+        return (
+            point[0] >= 0
+            and point[1] >= 0
+            and point[0] < self.nrows
+            and point[1] < self.ncols
+        )
+
+    def process_all_trailheads(self):
+        peaks = []
+        score = 0
+        for trailhead in self.trailheads:
+            # print("Processing trailhead {}".format(trailhead))
+            new_peaks = self.process_trailhead(trailhead)
+            _score = len(new_peaks)
+            # print("Score for this trailhead is {}".format(_score))
+            peaks += new_peaks
+            score += _score
+        # print("There are {} Peaks".format(len(peaks)))
+        print("Total score is {}".format(score))
+        # print("Here are all the peaks:\n", peaks)
 
 
-# file = "Inputs/day10_input.txt"
-file = "Inputs/day10_example.txt"
+file = "Inputs/day10_input.txt"
+# file = "Inputs/day10_example.txt"
 map = Map(file)
-map.print_peak_list()
-map.process_peak([0, 1])
+map.print_trailhead_list()
+# print(map.process_trailhead([0, 2]))
+map.process_all_trailheads()
+
+# 298 was the first answer and was too low
