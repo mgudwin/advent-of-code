af60b38706678f51dbcda076994e4794492d878c
Matt Gudwin <matt@Matts-MacBook-Pro.local>
2025-01-15 08:20:43 -0500

    Day 11 p2 in prog

--- day11.py
+++ day11.py
@@ -1,17 +1,5 @@
 import itertools
-from copy import deepcopy
-
-"""Day 11
-1. If the stone is engraved with the number 0, it is replaced by a stone 
-   engraved with the number 1.
-2. If the stone is engraved with a number that has an even number of digits,
-   it is replaced by two stones. The left half of the digits are engraved on
-   the new left stone, and the right half of the digits are engraved on the
-   new right stone. (The new numbers don't keep extra leading zeroes: 1000
-   would become stones 10 and 0.)
-3. If none of the other rules apply, the stone is replaced by a new stone;
-the old stone's number multiplied by 2024 is engraved on the new stone.
-"""
+import math
 
 
 class Stones:
@@ -19,6 +7,7 @@ class Stones:
 
     def __init__(self, file) -> None:
         self.stone_list = self.read_stones(file)
+        self.memoize_bitch = {}
 
     def read_stones(self, file):
         """Read stones"""
@@ -38,30 +27,37 @@ class Stones:
 
     def evaluate_stone(self, stone):
         """Process rules"""
-        len_of_stone = len(str(stone))
         # rule 1
         if stone == 0:
             return [1]
         # rule 2
+        len_of_stone = int(math.log10(stone)) + 1
         if len_of_stone % 2 == 0:
-            left_stone = int(str(stone)[0 : int(len_of_stone / 2)])
-            right_stone = int(str(stone)[int(len_of_stone / 2) :])
+            left_stone = stone // 10 ** (len_of_stone // 2)
+            right_stone = stone % 10 ** (len_of_stone // 2)
             return [left_stone, right_stone]
         return [stone * 2024]
 
     def blink(self):
         """During blink, process rules for each stone"""
-        for ix, stone in enumerate(self.stone_list):
-            _stone = self.stone_list.pop(ix)
-            self.stone_list.insert(ix, self.evaluate_stone(_stone))
-        self.stone_list = list(itertools.chain.from_iterable(self.stone_list))
+        new_stones = []
+        for stone in self.stone_list:
+            if stone == 0:
+                new_stones.extend([1])
+            elif stone in self.memoize_bitch:
+                new_stones.extend(self.memoize_bitch[stone])
+            else:
+                new_stone = self.evaluate_stone(stone)
+                new_stones.extend(new_stone)
+                self.memoize_bitch[stone] = new_stone
+        self.stone_list = new_stones
 
 
-pluto = Stones("Inputs/day11_input.txt")
-# pluto = Stones("Inputs/day11_example.txt")
+# pluto = Stones("Inputs/day11_input.txt")
+pluto = Stones("Inputs/day11_example.txt")
 pluto.print_stone_list()
 # for i in range(1, 7):
-for i in range(1, 26):
+for i in range(1, 76):
     print()
     print("==================================")
     print(f"After blink: {i}\n\n")
